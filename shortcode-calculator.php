33ea7fc4bb019fb240894bce6e21cefcde95fd3e
Johan van der Wijk <johan@vanderwijk.nl>
2020-08-10 11:46:54 +0200

    Witruimte verwijderd

--- shortcode-calculator.php
+++ shortcode-calculator.php
@@ -34,7 +34,6 @@ function calculator_shortcode( $atts ) {
 <p>Bij een gewenst maandinkomen bij arbeidsongeschiktheid van <strong><span id="desired-income-display" class="desired-income"></span></strong> bedraagt de maandelijkse inleg <strong><span id="montly-instalment" class="montly-instalment"></span></strong>.</p>
 <p>Het maandbedrag is inclusief de kosten van een privé Premium bunq account. Je spaarbedrag bedoeld voor schenkingen en de arbodienst is <span id="health_and_safety_service" class="health_and_safety_service"></span> en de bank en administratie kosten bedragen € 23,00. Deelnemers sinds de start in juli 2018 hebben van het spaarbedrag nog <?php echo esc_html ( get_option( 'surplus_percentage', 95 ) ); ?> % (<span class="surplus"></span>) over.</p>
 <p>*Indien je een eenmanszaak hebt is een maandelijks bruto inkomen van minimaal <strong><span id="required-income-eenmanszaak" class="required-income-eenmanszaak"></span></strong> benodigd en voor een BV bedraagt het minimum maandinkomen <strong><span id="required-income-bv" class="required-income-bv"></span></strong>.
-<p>&nbsp;</p>
 
 <?php
 	$ob_str = ob_get_contents();
